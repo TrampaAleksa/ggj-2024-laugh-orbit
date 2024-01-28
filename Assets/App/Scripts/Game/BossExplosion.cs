f30401e1cc21e1186af8ee2c5375427e2d843be7
Aleksa Trampa <alektramp@gmail.com>
2024-01-28 12:26:31 +0100

    Added boss explosion logic but havent connected

---
+++ Assets/App/Scripts/Game/BossExplosion.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class BossExplosion : MonoBehaviour
+{
+    public float delay;
+    
+    public void Start()
+    {
+        Invoke(nameof(Explode), delay);
+    }
+    
+    public void Explode()
+    {
+        var explosion = ExplosionPool.Instance.GetFromPool();
+        explosion.transform.position = transform.position;
+        explosion.gameObject.SetActive(true);
+    }
+}
---
+++ Assets/App/Scripts/Game/ExplosionSequence.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+public class ExplosionSequence : MonoBehaviour
+{
+    public PlayerCharacter playerCharacter;
+    private BossExplosion[] bossExplosions;
+
+    private void Awake()
+    {
+        bossExplosions = gameObject.GetComponentsInChildren<BossExplosion>();
+    }
+
+    public void BeginSequence()
+    {
+        foreach (var bossExplosion in bossExplosions)
+        {
+            bossExplosion.Explode();
+        }
+        
+        Invoke(nameof(EndSequence), 2f);
+    }
+
+    public void EndSequence()
+    {
+        var playerExplosion = ExplosionPool.Instance.GetFromPool();
+        playerExplosion.transform.position = playerCharacter.transform.position;
+        playerExplosion.gameObject.SetActive(true);
+        playerCharacter.GetComponent<SpriteRenderer>().enabled = false;
+    }
+}
