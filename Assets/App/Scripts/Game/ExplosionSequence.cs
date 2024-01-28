3ef5a660cf3854d7120b4017dd212e7f8edb8ee8
Aleksa Trampa <alektramp@gmail.com>
2024-01-28 14:08:26 +0100

    explosion sequence

--- Assets/App/Scripts/Game/ExplosionSequence.cs
+++ Assets/App/Scripts/Game/ExplosionSequence.cs
@@ -1,24 +1,36 @@
 ﻿using System;
+using DG.Tweening;
 using UnityEngine;
 
 public class ExplosionSequence : MonoBehaviour
 {
     public PlayerCharacter playerCharacter;
     private BossExplosion[] bossExplosions;
+    public float endSequenceDelay = 0.3f;
 
     private void Awake()
     {
         bossExplosions = gameObject.GetComponentsInChildren<BossExplosion>();
     }
 
-    public void BeginSequence()
+    private Action onEndSequenceAction;
+
+    public void BeginSequence(Action onEndSequence)
     {
+        onEndSequenceAction = onEndSequence;
+        
         foreach (var bossExplosion in bossExplosions)
         {
-            bossExplosion.Explode();
+            bossExplosion.StartExplosion();
         }
         
-        Invoke(nameof(EndSequence), 2f);
+        Invoke(nameof(DisableBossAfterFinalExplosion), 2f);
+        Invoke(nameof(EndSequence), endSequenceDelay);
+    }
+
+    private void DisableBossAfterFinalExplosion()
+    {
+        transform.parent.GetComponent<SpriteRenderer>().DOFade(0f, 0.3f).SetEase(Ease.InOutSine);
     }
 
     public void EndSequence()
@@ -26,6 +38,16 @@ public class ExplosionSequence : MonoBehaviour
         var playerExplosion = ExplosionPool.Instance.GetFromPool();
         playerExplosion.transform.position = playerCharacter.transform.position;
         playerExplosion.gameObject.SetActive(true);
-        playerCharacter.GetComponent<SpriteRenderer>().enabled = false;
+        playerCharacter.playerRenderer.DOFade(0f, 0.3f).SetEase(Ease.InOutSine);
+        
+        onEndSequenceAction?.Invoke();
     }
+
+    // public void Update()
+    // {
+    //     if (Input.GetKeyDown(KeyCode.Space))
+    //     {
+    //         BeginSequence();
+    //     }
+    // }
 }
